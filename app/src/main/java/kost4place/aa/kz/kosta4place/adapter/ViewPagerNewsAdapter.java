2eb4bf3fd91565780243cc377b1a35dcd996b5ff
YamamotaSempai <aidos.jantleu@gmail.com>
2018-09-06 08:03:02 +0600

    Added slider buttons

--- app/src/main/java/kost4place/aa/kz/kosta4place/adapter/ViewPagerNewsAdapter.java
+++ app/src/main/java/kost4place/aa/kz/kosta4place/adapter/ViewPagerNewsAdapter.java
@@ -8,15 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import kost4place.aa.kz.kosta4place.R;
 
 public class ViewPagerNewsAdapter extends PagerAdapter {
     private Context context;
     private LayoutInflater inflater;
-
     private Integer[] myImageList = {R.drawable.tux, R.drawable.tux,
-            R.drawable.tux, R.drawable.tux, R.drawable.tux};
+            R.drawable.tux};
 
     public ViewPagerNewsAdapter(Context context) {
         this.context = context;
@@ -34,12 +34,27 @@ public class ViewPagerNewsAdapter extends PagerAdapter {
 
     @NonNull
     @Override
-    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
 
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.special_slide_layout, null);
         ImageView imgView = view.findViewById(R.id.imageView);
         imgView.setImageResource(myImageList[position]);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (position == 0) {
+                    Toast.makeText(context, "1", Toast.LENGTH_SHORT).show();
+                }else if (position == 1) {
+                    Toast.makeText(context, "2", Toast.LENGTH_SHORT).show();
+                }else if (position == 2) {
+                    Toast.makeText(context, "3", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
         ViewPager vp = (ViewPager) container;
         vp.addView(view, 0);
--- app/src/main/java/kost4place/aa/kz/kosta4place/ui/MainActivity.java
+++ app/src/main/java/kost4place/aa/kz/kosta4place/ui/MainActivity.java
@@ -1,9 +1,16 @@
 package kost4place.aa.kz.kosta4place.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.viewpagerindicator.CirclePageIndicator;
 
@@ -17,17 +24,58 @@ import kost4place.aa.kz.kosta4place.adapter.ViewPagerNewsAdapter;
 public class MainActivity extends AppCompatActivity {
 
     private static ViewPager mPager;
+    private ViewPagerNewsAdapter viewPagerNewsAdapter;
+    private LinearLayout sliderDots;
+    private int dotsCount;
+    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sliderDots = findViewById(R.id.sliderDots); //заменить
         mPager = findViewById(R.id.viewPager);
 
-        ViewPagerNewsAdapter viewPagerNewsAdapter = new ViewPagerNewsAdapter(this);
-
+        viewPagerNewsAdapter = new ViewPagerNewsAdapter(this);
         mPager.setAdapter(viewPagerNewsAdapter);
+
+        dotsCount = viewPagerNewsAdapter.getCount();
+        dots = new ImageView[dotsCount];
+
+        for (int i = 0; i < dotsCount; i++) {
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.noactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+
+            sliderDots.addView(dots[i], params);
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < dots.length; i++) {
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.noactive_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTaskViewPager(), 2000, 4000);
@@ -40,23 +88,35 @@ public class MainActivity extends AppCompatActivity {
             MainActivity.this.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    for (int i = 0; i < mPager.getScrollBarSize(); i++) {
-
-                        if (i != mPager.getScrollBarSize()) {
-                            mPager.setCurrentItem(++i);
-                        } else {
-                            mPager.setCurrentItem(0);
-                        }
+                    if (mPager.getCurrentItem() == 0) {
+                        mPager.setCurrentItem(1);
+                    }else if (mPager.getCurrentItem() == 1) {
+                        mPager.setCurrentItem(2);
+                    }else if (mPager.getCurrentItem() == 2) {
+                        mPager.setCurrentItem(3);
                     }
-//                    for (int i = 0; i < mPager.getScrollBarSize(); i++) {
-//                        if (i == mPager.getScrollBarSize()){
-//                            i = 0;
-//                        } else {
-//                            mPager.setCurrentItem(++i);
-//                        }
-//                    }
                 }
             });
         }
     }
 }
+
+
+
+//for (int i = 0; i < 3; i++) {
+//        switch (i) {
+//        case 0:
+//        mPager.setCurrentItem(i);
+//        break;
+//        case 1:
+//        mPager.setCurrentItem(i);
+//        break;
+//        case 2:
+//        mPager.setCurrentItem(i);
+//        break;
+//        case 3:
+//        mPager.setCurrentItem(i);
+//        i = 0;
+//        break;
+//        }
+//        }
