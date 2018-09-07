8a2501c288ba177276a7212ac01e5857ebc534f3
YamamotaSempai <aidos.jantleu@gmail.com>
2018-09-07 07:36:43 +0600

    Цикл сделан

--- app/src/main/java/kost4place/aa/kz/kosta4place/ui/MainActivity.java
+++ app/src/main/java/kost4place/aa/kz/kosta4place/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -17,24 +18,27 @@ import com.viewpagerindicator.CirclePageIndicator;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import kost4place.aa.kz.kosta4place.R;
 import kost4place.aa.kz.kosta4place.adapter.ViewPagerNewsAdapter;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
     private static ViewPager mPager;
     private ViewPagerNewsAdapter viewPagerNewsAdapter;
     private LinearLayout sliderDots;
     private int dotsCount;
     private ImageView[] dots;
 
+    private String nameClass = MainActivity.class.getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sliderDots = findViewById(R.id.sliderDots); //заменить
+        sliderDots = findViewById(R.id.sliderDots);
         mPager = findViewById(R.id.viewPager);
 
         viewPagerNewsAdapter = new ViewPagerNewsAdapter(this);
@@ -43,6 +47,16 @@ public class MainActivity extends AppCompatActivity {
         dotsCount = viewPagerNewsAdapter.getCount();
         dots = new ImageView[dotsCount];
 
+        //Method which does paging sliders dots
+        setSliderDots();
+
+        mPager.addOnPageChangeListener(this);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTaskViewPager(), 2000, 4000);
+    }
+
+    private void setSliderDots() {
         for (int i = 0; i < dotsCount; i++) {
             dots[i] = new ImageView(this);
             dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.noactive_dot));
@@ -53,36 +67,10 @@ public class MainActivity extends AppCompatActivity {
 
             sliderDots.addView(dots[i], params);
         }
-
         dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
-
-        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                for (int i = 0; i < dots.length; i++) {
-                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.noactive_dot));
-                }
-
-                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTaskViewPager(), 2000, 4000);
     }
 
     public class TimerTaskViewPager extends TimerTask {
-//todo ОБНОВА ПИЗЛДУЦ
         @Override
         public void run() {
             MainActivity.this.runOnUiThread(new Runnable() {
@@ -90,34 +78,32 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                     if (mPager.getCurrentItem() == 0) {
                         mPager.setCurrentItem(1);
-                    }else if (mPager.getCurrentItem() == 1) {
+                    } else if (mPager.getCurrentItem() == 1) {
                         mPager.setCurrentItem(2);
-                    }else if (mPager.getCurrentItem() == 2) {
-                        mPager.setCurrentItem(3);
+                    } else if (mPager.getCurrentItem() == 2) {
+                        mPager.setCurrentItem(0);
                     }
-
                 }
             });
         }
     }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        for (int i = 0; i < dots.length; i++) {
+        dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.noactive_dot));
+        }
+
+        dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
 }
-
-
-
-//for (int i = 0; i < 3; i++) {
-//        switch (i) {
-//        case 0:
-//        mPager.setCurrentItem(i);
-//        break;
-//        case 1:
-//        mPager.setCurrentItem(i);
-//        break;
-//        case 2:
-//        mPager.setCurrentItem(i);
-//        break;
-//        case 3:
-//        mPager.setCurrentItem(i);
-//        i = 0;
-//        break;
-//        }
-//        }
