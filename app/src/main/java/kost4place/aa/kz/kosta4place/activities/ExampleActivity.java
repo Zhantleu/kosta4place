f991b25d7ea7bdb50abef8b34e889a8473cffb74
YamamotaSempai <aidos.jantleu@gmail.com>
2018-11-11 01:08:39 +0600

    socket img

--- app/src/main/java/kost4place/aa/kz/kosta4place/activities/ExampleActivity.java
+++ app/src/main/java/kost4place/aa/kz/kosta4place/activities/ExampleActivity.java
@@ -2,43 +2,16 @@ package kost4place.aa.kz.kosta4place.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 import kost4place.aa.kz.kosta4place.R;
-import kost4place.aa.kz.kosta4place.repository.PlaceRepository;
 import kost4place.aa.kz.kosta4place.utils.SocketConnection;
 
 public class ExampleActivity extends AppCompatActivity {
 
-    EditText info;
-    EditText titlePlace;
-    EditText location;
-    RecyclerView recyclerView;
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main1);
-//        info = findViewById(R.id.et_firstname);
-//        titlePlace = findViewById(R.id.et_lastname);
-//        location = findViewById(R.id.et_location);
-//        recyclerView = findViewById(R.id.recycler);
-//
-//        PlaceRepository placeRepository = new PlaceRepository();
-//        placeRepository.getPlaces(getApplicationContext())
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe();
-//    }
-
-    TextView response;
-    EditText editTextAddress, editTextPort;
+    //todo Сделать вывод картинки Base64
+    ImageView response;
     Button buttonConnect, buttonClear;
 
     @Override
@@ -46,19 +19,17 @@ public class ExampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.socket_example);
 
-        editTextAddress = findViewById(R.id.addressEditText);
-        editTextPort = findViewById(R.id.portEditText);
         buttonConnect = findViewById(R.id.connectButton);
         buttonClear = findViewById(R.id.clearButton);
         response = findViewById(R.id.responseTextView);
 
+        SocketConnection myClient = new SocketConnection(response,this);
+        myClient.execute();
+
         buttonConnect.setOnClickListener(arg0 -> {
-            SocketConnection myClient = new SocketConnection(editTextAddress.getText()
-                    .toString(), Integer.parseInt(editTextPort
-                    .getText().toString()), response);
-            myClient.execute();
+
         });
 
-        buttonClear.setOnClickListener(v -> response.setText(""));
+//        buttonClear.setOnClickListener(v -> response.clearColorFilter());
     }
 }
--- app/src/main/java/kost4place/aa/kz/kosta4place/utils/SocketConnection.java
+++ app/src/main/java/kost4place/aa/kz/kosta4place/utils/SocketConnection.java
@@ -1,75 +1,101 @@
 package kost4place.aa.kz.kosta4place.utils;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.widget.TextView;
+import android.util.Base64;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class SocketConnection extends AsyncTask<Void, Void, String> {
 
-    String dstAddress;
-    int dstPort;
-    String response = "";
-    TextView textResponse;
+    private static final String IP = "192.168.1.245";
+    private static final int PORT = 8889;
 
-    public SocketConnection(String addr, int port, TextView textResponse) {
-        dstAddress = addr;
-        dstPort = port;
+    private StringBuilder stringBuilder = new StringBuilder();
+    private ImageView textResponse;
+    private Context context;
+
+    public SocketConnection(ImageView textResponse, Context context) {
         this.textResponse = textResponse;
+        this.context = context;
     }
 
     @Override
     protected String doInBackground(Void... arg0) {
 
-        Socket socket = null;
+        try (Socket socket = new Socket(IP, PORT)) {
+            try (PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-        try {
-            socket = new Socket(dstAddress, dstPort);
+                out.println("1");
+                out.flush();
 
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
-                    1024);
-            byte[] buffer = new byte[1024];
-
-            int bytesRead;
-            InputStream inputStream = socket.getInputStream();
-
-            /*
-             * notice: inputStream.read() will block if no data return
-             */
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                byteArrayOutputStream.write(buffer, 0, bytesRead);
-                response += byteArrayOutputStream.toString("UTF-8");
-            }
-
-        } catch (UnknownHostException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            response = "UnknownHostException: " + e.toString();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            response = "IOException: " + e.toString();
-        } finally {
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+                int c;
+                while ((c = in.read()) != -1) {
+                    System.out.print((char) c);
+                    stringBuilder.append((char) c);
                 }
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return response;
+        return stringBuilder.toString();
     }
 
     @Override
     protected void onPostExecute(String result) {
-        textResponse.setText(response);
-        super.onPostExecute(result);
+
+        byte[] decoded = Base64.decode(result, Base64.DEFAULT);
+
+        Bitmap bitmap = BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+
+        Glide.with(context)
+                .load(stream.toByteArray())
+                .asBitmap()
+                .into(textResponse);
+
+    }
+
+
+    private String saveToInternalStorage(Bitmap bitmapImage, Context context) {
+        ContextWrapper cw = new ContextWrapper(context);
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath = new File(directory, "profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
     }
 
 }
